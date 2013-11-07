712b21dc7a3d115e3a57c3c52f512f1c2528668c
saintchang <saint.chang@gmail.com>
2013-11-07 21:31:40 +0800

    modify

--- test/ApplicationTest.php
+++ test/ApplicationTest.php
@@ -1,15 +1,21 @@
 <?php
 
-class ApplicationTest extends PHPUnit_Framework_TestCase
+namespace saintmvc\test;
+
+class ApplicationTest extends \PHPUnit_Framework_TestCase
 {
 
 
     protected $obj;
  
+    /* 
     protected function setUp()
     {
         $this->obj = new application();   
     }
- 
+    */
+
+    public function testStartPath()
+    {}
 }
 ?>
